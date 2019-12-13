ffc1fc109e114d3fde51e667fbb2eb3df2ce7f71
everedx <carlos.serranoe1992@gmail.com>
2019-12-13 23:32:01 +1100

    Initial commit

---
+++ Assets/Scripts/AndroidTesting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        // Handle screen touches.
+        if (Input.touchCount > 0)
+        {
+            // Debug.Log(Screen.width + " " + Screen.height);
+            //Debug.Log(Camera.current.orthographicSize);
+            Touch touch = Input.GetTouch(0);
+            //Debug.Log("X:"+touch.position.x + " Y:" + touch.position.y);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Camera m_OrthographicCamera;
+    Rigidbody2D rbody;
+    public float speed = 5.0f;
+    private float reductionFactor = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        reductionFactor = m_OrthographicCamera.orthographicSize / 2;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Handle screen touches.
+        if (Input.touchCount > 0)
+        {
+            Vector2 origin = transform.position;
+            Touch touch = Input.GetTouch(0);
+            Vector2 destination = m_OrthographicCamera.ScreenToWorldPoint(touch.position);
+            
+            //IF DISTANCE > reductionFactor, THEN SPEED = MAX, else the speed varies
+            float xDistance = transform.position.x - destination.x; //If positive, we need to go left
+            float yDistance = transform.position.y - destination.y; //If positive we need to go down
+
+            xDistance = -xDistance / reductionFactor;
+            yDistance = -yDistance / reductionFactor;
+            Debug.Log("X:"+ xDistance + " Y:"+ yDistance);
+            if (Mathf.Abs(xDistance) > 1)
+            {
+                xDistance = (xDistance > 0) ?  1 : -1;
+            }
+            if (Mathf.Abs(yDistance) > 1)
+            {
+                yDistance = (yDistance > 0) ? 1 : -1;
+            }
+            Debug.Log("X:" + xDistance + " Y:" + yDistance);
+            Vector2 move = new Vector2(xDistance,yDistance);
+
+            origin = origin + move * speed * Time.deltaTime;
+
+            rbody.MovePosition(origin);
+            //m_OrthographicCamera.ScreenToWorldPoint
+        }
+    }
+}
