1d37481c873492cf307fb344439d41e1d4f704e7
everedx <carlos.serranoe1992@gmail.com>
2020-03-16 21:00:19 +1100

    Fixed Small bug with last level
    Now the next level button appears disabled

--- Assets/Scripts/FinishMenu.cs
+++ Assets/Scripts/FinishMenu.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FinishMenu : MonoBehaviour
 {
@@ -15,6 +17,13 @@ public class FinishMenu : MonoBehaviour
     public void showFinishMenu()
     {
         animator.SetTrigger("showFinishMenu");
+     
+    }
+    public void hideNextIfLast()
+    {
+        string path = SceneUtility.GetScenePathByBuildIndex(SceneManager.GetActiveScene().buildIndex + 1);
+        if (string.IsNullOrEmpty(path))
+            GameObject.Find("ButtonNext").GetComponent<Button>().enabled = false;
     }
 
 }
